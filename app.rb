11b891f0d1b22d7d08ea13ad5f235bf4f5f1228c
Harry Chen <harry@harry.gg>
2015-08-11 11:48:31 +0800

    feat(songs): New path and views for songs

--- app.rb
+++ app.rb
@@ -2,4 +2,14 @@ class FirstSinatraApp < Sinatra::Base
   get "/" do
     erb "WDI is awesome."
   end
+
+  get "/songs" do
+    @songs = [
+      "Fly Me To The Moon",
+      "New York, New York",
+      "Luck Be A Lady"
+    ]
+
+    erb :'songs' # this is the path to our view file!
+  end
 end
